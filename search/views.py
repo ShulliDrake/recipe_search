29a49fce0d964e48b8197809aeda6593c7221c20
nozomi <nozomisumiya@gmail.com>
2013-04-24 09:18:57 -0700

    home page bare bones

--- search/views.py
+++ search/views.py
@@ -4,6 +4,6 @@ from django.template import RequestContext
 
 def search(request):
 
-    return render_to_response("base.html",
+    return render_to_response("home.html",
                               {},
                               context_instance=RequestContext(request))
